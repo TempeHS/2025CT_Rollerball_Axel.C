acbee199d706777c73ef692a821a8956220ae440
Axel C <axel.clark@education.nsw.gov.au>
2025-03-06 09:39:20 +1100

    Added PBR material for floor and started player control script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,10 +10,11 @@ public class PlayerController : MonoBehaviour
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void OnMove(InputValue movementValue)
     {
+        Vector2 movementVector = movementValue.Get<Vector2>();
         
     }
+
 }
