a784ac5f6ab6f2ea1c2321e939f8cb775e9e9a60
TheSpiciestRamen <159206642+TheSpiciestRamen@users.noreply.github.com>
2025-04-14 13:46:24 +1000

    Added dynamic ground objects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -38,6 +38,7 @@ public class PlayerController : MonoBehaviour
         if(count >= 15)
         {
             winTextObject.SetActive(true); 
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
 
         }
 
@@ -50,6 +51,17 @@ public class PlayerController : MonoBehaviour
 
         rb.AddForce(movement * speed);
 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            winTextObject.gameObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+
+        }
     }
 
 
